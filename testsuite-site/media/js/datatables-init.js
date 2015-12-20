fa8268f04a8663c49bcf6e3874bfb7358896ecac
Matt Garrish <matt.garrish@gmail.com>
2015-12-20 10:02:41 -0500

    create prototype function for calling datatables, add option to not sort a table by default

--- testsuite-site/media/js/datatables-init.js
+++ testsuite-site/media/js/datatables-init.js
@@ -1,98 +1,107 @@
 
-    function initDataTable(fixedHeader,fixedCol,searchable,changeDefaultSort,srchLabel,srchPlaceholder) {
-        var tables = document.getElementsByTagName('table');
-        
-        // add table ids to a non-dynamic list (datatables will grow the # of tables as they're made dynamic)
-        var ids = new Array(tables.length-1);
-        for (var i = 0; i < tables.length; i++) {
-            ids[i] = tables[i].id;
-        }
-        
-        for (var j = 0; j < ids.length; j++) {
-            makeDynamic(ids[j], fixedHeader, fixedCol, searchable, srchLabel, srchPlaceholder);
-            if (changeDefaultSort) {
-               changeSort(ids[j]);
-            }
+function DT(options) {
+    this.fixedHeader = options.fixedHeader !== undefined ? Boolean(options.fixedHeader) : true;
+    this.fixedCol = options.fixedColumn !== undefined ? Boolean(options.fixedColumn) : false;
+    this.searchable = options.searchable !== undefined ? Boolean(options.searchable) : true;
+    this.setDefaultSort = options.setDefaultSort !== undefined ? Boolean(options.setDefaultSort) : true;
+    this.changeDefaultSort = options.changeDefaultSort !== undefined ? Boolean(options.changeDefaultSort) : true;
+    this.srchLabel = (options.srchLabel == undefined || options.srchLabel == null || options.srchLabel != '') ? 'Find Reading System(s) ' : options.srchLabel;
+    this.srchPlaceholder = (options.srchPlaceholder == undefined || options.srchLabel == null || options.srchPlaceholder == '') ? 'Enter a name or operating system' : options.srchPlaceholder;
+}
+
+DT.prototype.initTables = function() {
+    var tables = document.getElementsByTagName('table');
+    
+    // add table ids to a non-dynamic list (datatables will grow the # of tables as they're made dynamic)
+    var ids = new Array(tables.length-1);
+    for (var i = 0; i < tables.length; i++) {
+        ids[i] = tables[i].id;
+    }
+    
+    for (var j = 0; j < ids.length; j++) {
+        this.makeDynamic(ids[j]);
+        if (this.changeDefaultSort) {
+           this.changeSort(ids[j]);
         }
     }
-        
-    function makeDynamic(tblID, fixedHeader, fixedCol, searchable, srchLabel, srchPlaceholder) {
+}
+
+DT.prototype.makeDynamic = function(tblID) {
+
+    var isMobile = window.matchMedia("only screen and (max-width: 760px)");
     
-        var sLabel = srchLabel == '' ? 'Find Reading System(s) ' : srchLabel;
-        var sPlaceholder = srchPlaceholder == '' ? 'Enter a name or operating system' : srchPlaceholder;
-        
-        var isMobile = window.matchMedia("only screen and (max-width: 760px)");
-        
-        var table;
-        
-        if (isMobile.matches) {
-            $('#'+tblID).DataTable({
+    var table;
+    
+    if (isMobile.matches) {
+        $('#'+tblID).DataTable({
+            "paging": false,
+            "info": false,
+            "stateSave": true,
+            "responsive": {
+                "details": {
+                    "display": $.fn.dataTable.Responsive.display.childRowImmediate,
+                    "type": ''
+                }
+            },
+            "oLanguage": {
+                "sSearch": this.srchLabel,
+                "sSearchPlaceholder": this.srchPlaceholder
+            }
+        });
+    }
+    else {
+        if (this.fixedCol) {
+            table = $('#'+tblID).DataTable({
                 "paging": false,
                 "info": false,
+                "searching": this.searchable,
                 "stateSave": true,
-                "responsive": {
-                    "details": {
-                        "display": $.fn.dataTable.Responsive.display.childRowImmediate,
-                        "type": ''
-                    }
+                "autoWidth": false,
+                "scrollY": '50vh',
+                "scrollX": true,
+                "scrollCollapse": false,
+                "fixedColumns": {
+                    "leftColumns": 1
                 },
                 "oLanguage": {
-                    "sSearch": sLabel,
-                    "sSearchPlaceholder": sPlaceholder
+                    "sSearch": this.srchLabel,
+                    "sSearchPlaceholder": this.srchPlaceholder
                 }
             });
         }
         else {
-            if (fixedCol) {
-                table = $('#'+tblID).DataTable({
-                    "paging": false,
-                    "info": false,
-                    "searching": searchable,
-                    "stateSave": true,
-                    "autoWidth": false,
-                    "scrollY": '50vh',
-                    "scrollX": true,
-                    "scrollCollapse": false,
-                    "fixedColumns": {
-                        "leftColumns": 1
-                    },
-                    "oLanguage": {
-                        "sSearch": sLabel,
-                        "sSearchPlaceholder": sPlaceholder
-                    }
-                });
-            }
-            else {
-                table = $('#'+tblID).DataTable({
-                    "paging": false,
-                    "info": false,
-                    "searching": searchable,
-                    "stateSave": true,
-                    "autoWidth": false,
-                    "oLanguage": {
-                        "sSearch": sLabel,
-                        "sSearchPlaceholder": sPlaceholder
-                    }
-                });
-            }
-        }
-        
-        if (!isMobile.matches && fixedHeader) {
-            new $.fn.dataTable.FixedHeader( table, {
-                // options
+            table = $('#'+tblID).DataTable({
+                "paging": false,
+                "info": false,
+                "searching": this.searchable,
+                "stateSave": true,
+                "autoWidth": false,
+                "aaSorting": this.setDefaultSort ? [0,'asc'] : [],
+                "oLanguage": {
+                    "sSearch": this.srchLabel,
+                    "sSearchPlaceholder": this.srchPlaceholder
+                }
             });
         }
     }
     
-    function changeSort(tblID) {
-        var oTable = $('#'+tblID).dataTable();
-        var oSettings = oTable.fnSettings();
-        var iColumns = oSettings.aoColumns.length - 1;
-        
-        for (var i = 0; i <= iColumns; i++) {
-            var colName = oSettings.aoColumns[i].sTitle.toLowerCase();
-            if (colName != '#' && colName != 'name' && colName != 'at' && colName != 'description') {
-                oSettings.aoColumns[i].asSorting = ['desc','asc'];
-            }
+    if (!isMobile.matches && this.fixedHeader) {
+        new $.fn.dataTable.FixedHeader( table, {
+            // options
+        });
+    }
+}
+
+DT.prototype.changeSort = function(tblID) {
+    var oTable = $('#'+tblID).dataTable();
+    var oSettings = oTable.fnSettings();
+    var iColumns = oSettings.aoColumns.length - 1;
+    
+    for (var i = 0; i <= iColumns; i++) {
+        var colName = oSettings.aoColumns[i].sTitle.toLowerCase();
+        if (colName != '#' && colName != 'name' && colName != 'at' && colName != 'description') {
+            oSettings.aoColumns[i].asSorting = ['desc','asc'];
         }
     }
+}
+ 
