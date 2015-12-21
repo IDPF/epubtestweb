ca238973d089cec5c31c0898c55e1db6fec3d2f8
Matt Garrish <matt.garrish@gmail.com>
2015-12-21 01:10:00 -0500

    remove mobile table view, simplify datatables initialization, add table view change option to pages

--- testsuite-site/media/js/datatables-init.js
+++ testsuite-site/media/js/datatables-init.js
@@ -1,5 +1,7 @@
 
-function DT(options) {
+function DT() { }
+
+DT.prototype.initialize = function(options) {
     this.fixedHeader = options.fixedHeader !== undefined ? Boolean(options.fixedHeader) : true;
     this.fixedCol = options.fixedColumn !== undefined ? Boolean(options.fixedColumn) : false;
     this.searchable = options.searchable !== undefined ? Boolean(options.searchable) : true;
@@ -8,39 +10,54 @@ function DT(options) {
     this.srchLabel = (options.srchLabel == undefined || options.srchLabel == null || options.srchLabel == '') ? 'Find Reading System(s) ' : options.srchLabel;
     this.srchPlaceholder = (options.srchPlaceholder == undefined || options.srchLabel == null || options.srchPlaceholder == '') ? 'Enter a name or operating system' : options.srchPlaceholder;
     this.isMobile = window.matchMedia("only screen and (max-device-width: 760px)").matches;
+    this.tableType = options.tableType;
+    
+    this.enhance();
 }
 
-DT.prototype.initTables = function() {
+DT.prototype.enhance = function() {
     var tables = document.getElementsByTagName('table');
     
     // add table ids to a non-dynamic list (datatables will grow the # of tables as they're made dynamic)
-    var ids = new Array(tables.length-1);
+    var IDs = [];
     for (var i = 0; i < tables.length; i++) {
-        ids[i] = tables[i].id;
-    }
-    
-    for (var j = 0; j < ids.length; j++) {
-        this.makeDynamic(ids[j]);
-        if (this.changeDefaultSort) {
-           this.changeSort(ids[j]);
+        var thisID = tables[i].id;
+        if (typeof thisID !== undefined && thisID !== null && thisID != '') {
+            IDs.push(thisID);
         }
     }
+    
+    for (var j = 0; j < IDs.length; j++) {
+        this.makeDynamic(IDs[j]);
+        if (this.changeDefaultSort) {
+           this.changeSort(IDs[j]);
+        }
+    }
+    
+    // set the table view linking + cookie
+    this.setUserView();
 }
 
 DT.prototype.makeDynamic = function(tblID) {
 
+    if ( $.fn.DataTable.isDataTable('#'+tblID) ) {
+        $('#'+tblID).DataTable().destroy();
+    }
+    
     var table;
     
-    if (this.isMobile) {
-        $('#'+tblID).DataTable({
+    if (this.fixedCol) {
+        table = $('#'+tblID).DataTable({
             "paging": false,
             "info": false,
+            "searching": this.searchable,
             "stateSave": true,
-            "responsive": {
-                "details": {
-                    "display": $.fn.dataTable.Responsive.display.childRowImmediate,
-                    "type": ''
-                }
+            "autoWidth": false,
+            "scrollY": '50vh',
+            "scrollX": true,
+            "scrollCollapse": false,
+            "fixedColumns": {
+                "leftColumns": 1
             },
             "oLanguage": {
                 "sSearch": this.srchLabel,
@@ -49,42 +66,21 @@ DT.prototype.makeDynamic = function(tblID) {
         });
     }
     else {
-        if (this.fixedCol) {
-            table = $('#'+tblID).DataTable({
-                "paging": false,
-                "info": false,
-                "searching": this.searchable,
-                "stateSave": true,
-                "autoWidth": false,
-                "scrollY": '50vh',
-                "scrollX": true,
-                "scrollCollapse": false,
-                "fixedColumns": {
-                    "leftColumns": 1
-                },
-                "oLanguage": {
-                    "sSearch": this.srchLabel,
-                    "sSearchPlaceholder": this.srchPlaceholder
-                }
-            });
-        }
-        else {
-            table = $('#'+tblID).DataTable({
-                "paging": false,
-                "info": false,
-                "searching": this.searchable,
-                "stateSave": true,
-                "autoWidth": false,
-                "aaSorting": this.setDefaultSort ? [0,'asc'] : [],
-                "oLanguage": {
-                    "sSearch": this.srchLabel,
-                    "sSearchPlaceholder": this.srchPlaceholder
-                }
-            });
-        }
+        table = $('#'+tblID).DataTable({
+            "paging": false,
+            "info": false,
+            "searching": this.searchable,
+            "stateSave": true,
+            "autoWidth": false,
+            "aaSorting": this.setDefaultSort ? [0,'asc'] : [],
+            "oLanguage": {
+                "sSearch": this.srchLabel,
+                "sSearchPlaceholder": this.srchPlaceholder
+            }
+        });
     }
     
-    if (!this.isMobile && this.fixedHeader) {
+    if (this.fixedHeader) {
         new $.fn.dataTable.FixedHeader( table, {
             // options
         });
@@ -103,4 +99,31 @@ DT.prototype.changeSort = function(tblID) {
         }
     }
 }
- 
+
+DT.prototype.setUserView = function() {    
+    if (this.tableType == 'col') {
+        Cookies.set('tblType', 'col', { expires: 365 });
+        this.setTableViewLinks('col','hd');
+    }
+    else {
+        Cookies.set('tblType', 'hd', { expires: 365 });
+        this.setTableViewLinks('hd','col');
+    }
+}
+
+DT.prototype.setTableViewLinks = function(active, inactive) {
+    $('#'+active).unbind('click');
+    $('#'+active).addClass('inactiveLink');
+    $('#'+inactive).bind('click');
+    $('#'+inactive).removeClass('inactiveLink');
+}
+
+DT.prototype.getTableType = function() {
+    var tblType = Cookies.get('tblType'); ;
+    
+    if (typeof(tblType) == 'undefined' || tblType == null || tblType == '') {
+        Cookies.set('tblType', 'hd', { expires: 365 });
+    }
+    
+    return tblType;
+}
