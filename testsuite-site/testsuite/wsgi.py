8e47df14fb5515d0e4eef0c22e6630044c6ad3c8
Marisa DeMeglio <marisa.demeglio@gmail.com>
2016-01-18 00:56:15 +0000

    experiments with wsgi

--- testsuite-site/testsuite/wsgi.py
+++ testsuite-site/testsuite/wsgi.py
@@ -15,11 +15,24 @@ framework.
 """
 import os
 
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/home/djangoweb/epubtest.org/epubtestweb/env/local/lib/python3.4/site-packages')
+
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/djangoweb/epubtest.org/epubtestweb/')
+sys.path.append('/home/djangoweb/epubtest.org/epubtestweb/testsuite-site/')
+
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "testsuite.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testsuite.settings")
+
+
+# Activate your virtual env
+activate_env=os.path.expanduser("/home/djangoweb/epubtest.org/epubtestweb/env/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
